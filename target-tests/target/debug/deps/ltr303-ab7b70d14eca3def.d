50356fe797a726704278177f6a70ddba481e4fc3
Ardelean Calin <ardelean.calin@proton.me>
2022-06-18 11:48:43 +0000

    Removed unused files.

--- target-tests/target/debug/deps/ltr303-ab7b70d14eca3def.d
+++
@@ -1,5 +0,0 @@
-/home/calin/Projects/ltr303/target-tests/target/debug/deps/ltr303-ab7b70d14eca3def: tests/ltr303.rs
-
-/home/calin/Projects/ltr303/target-tests/target/debug/deps/ltr303-ab7b70d14eca3def.d: tests/ltr303.rs
-
-tests/ltr303.rs:
